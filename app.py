c66b81b541c7692dcbb387e88ea566ac093231f3
BajojiAdithyasena <bajojiadithyasen@gmail.com>
2026-06-27 13:43:30 +0530

    added name and room submission but still have no idea what i am doing

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "fhffhffhfjsnnsfm"
 socketio = SocketIO(app)
-
+rooms = []
 @app.route("/",methods=["POST","GET"])
 def home():
     return render_template("home.html")
 
 @app.route("/standby",methods=["POST","GET"])
 def standby():
+    name = request.args.get("name")
+    room = request.args.get("room")
+    print(name,room)
+    rooms.append({"host":name,"room":room})
     return render_template("standby.html")
 
 
